4822da45a128d071d5293f4f86e38a0e5461c0ed
x1nyuan <yao.ntno@gmail.com>
2018-12-22 04:44:29 +0900

    first serve

--- app/onikuflow/__init__.py
+++ app/onikuflow/__init__.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import logging
-from flask import Flask
+from .view import api_bp
 from .utils.encorders import DefaultEncoder
-
-app = Flask(__name__)
-app.json_encoder = DefaultEncoder
-
-from . import view
+from flask import Flask
+import logging
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
 )
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.json_encoder = DefaultEncoder
+    app.register_blueprint(api_bp)
+    return app
--- app/onikuflow/view.py
+++ app/onikuflow/view.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from . import app
+from flask import Blueprint
 from flask_restful import Api
 from .webapi import QueryAPI
 
 
-api = Api(app)
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
 api.add_resource(QueryAPI, '/query')
+
+
