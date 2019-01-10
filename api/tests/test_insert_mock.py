4e4dca8fd7b0687b4932d064e882a7f905800ac3
xinyuan yao <yao.ntno@gmail.com>
2019-01-10 11:00:58 +0000

    add demo data

---
+++ api/tests/test_insert_mock.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+from mlboard_client.writer import SummaryWriter
+import random
+
+
+@pytest.mark.parametrize("id", [1, 2, 3])
+def test_all_table(id):
+    writer = SummaryWriter("http://api:5000", f'test_{id}')
+    writer.update_config({'aaa': 'bbb'})
+    for i in range(10):
+        writer.add_scalar('metric-0', random.uniform(0, 1), i)
+        writer.add_scalar('metric-1', random.uniform(0, 1), i)
+        writer.add_scalar('metric-2', random.uniform(0, 1), i)
