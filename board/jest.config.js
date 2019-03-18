1cce7f0d2c888f8e139b1a6201fc6eafb88476a1
xinyuan yao <yao.ntno@gmail.com>
2019-03-18 13:17:29 +0000

    add jest

--- board/jest.config.js
+++ board/jest.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   verbose: true,
+  moduleFileExtensions: [
+    "js",
+    "json",
+    // tell Jest to handle `*.vue` files
+    "vue"
+  ],
   transform: {
     "^.+\\.jsx$": "babel-jest",
-    "^.+\\.js$": "babel-jest"
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.vue$": "vue-jest"
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
   },
   collectCoverage: true,
 };
