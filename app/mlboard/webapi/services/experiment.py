8ce9678dd52af9ebe5ecb337caf099b3c17ddd78
xinyuan yao <yao.ntno@gmail.com>
2019-04-22 13:11:08 +0000

    add experiment all api

--- app/mlboard/webapi/services/experiment.py
+++ app/mlboard/webapi/services/experiment.py
@@ -2,23 +2,34 @@ from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 from mlboard.orm import queries as qs
 from logging import getLogger
-from ..encoders import StrictEncoder
+from cytoolz.curried import pipe, map
 from pydantic import BaseModel
+from datetime import datetime
 from typing import List, Dict, Any, Optional
-import os
 import uuid
 logger = getLogger("api")
 
 router = APIRouter()
+
 
 class Experiment(BaseModel):
     id: uuid.UUID
     name: str
     memo: Optional[str]
     config: Dict
+    create_date: datetime
+
 
 @router.get('/experiment/all', response_model=List[Experiment])
-def all():
-    res = qs.Experiment().all()
-    aa  = Experiment()
-    return aa
+async def all():
+    res = pipe(
+        qs.Experiment().all(),
+        map(lambda x: Experiment(
+            id=x.id,
+            name=x.name,
+            memo=x.memo,
+            config=x.config,
+        )),
+        list
+    )
+    return res
--- app/tests/webapi/test_experiment.py
+++ app/tests/webapi/test_experiment.py
@@ -1,7 +1,17 @@
 import pytest
 from .fixtures import client
+from mlboard.orm import models as ms
+from unittest import mock
+
+NAMESPACE = 'mlboard.webapi.services.experiment'
 
 
-def test_all(client):
-    res = client.post('/experiment/all')
+@mock.patch(f'{NAMESPACE}.qs.Experiment')
+def test_all(mock_qury, client):
+    dummy_item = ms.Experiment(
+        name='foo',
+    )
+    mock_qury().all = mock.MagicMock(return_value=[dummy_item])
+    res = client.get('/experiment/all')
     assert res.status_code == 200
+    assert res.json()[0]['name'] == dummy_item.name
