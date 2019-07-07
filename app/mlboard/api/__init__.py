0803e3d672b7a02e5490bdfe35dd9dccedcc03b1
908kre <ce51046.higuchi@gmail.com>
2019-07-07 06:08:53 +0000

    rm api

--- app/mlboard/api/__init__.py
+++ app/mlboard/api/__init__.py
@@ -1,15 +1,8 @@
 #  from flask import Flask
 #  from ..config import config
 from fastapi import FastAPI
-from .services import process
-from .services import chamber
 from .services import sensor
-from .services import trace_level
-from .services import event_section
 from .services import trace
-from .services import maintenance_log
-from .services import maintenance
-from .services import trace_event
 from mlboard.core import db
 import asyncio
 from logging import getLogger, Formatter, StreamHandler, DEBUG
@@ -50,13 +43,6 @@ def create_app() -> FastAPI:
     app.redoc_url = None
     app.on_event("startup")(startup)
     app.on_event("shutdown")(shutdown)
-    app.include_router(process.router, tags=['process'])
-    app.include_router(chamber.router, tags=['chamber'])
     app.include_router(sensor.router, tags=['sensor'])
-    app.include_router(trace_level.router, tags=['trace_level'])
-    app.include_router(event_section.router, tags=['event_section'])
     app.include_router(trace.router, tags=['trace'])
-    app.include_router(maintenance_log.router, tags=['maintenance_log'])
-    app.include_router(maintenance.router, tags=['maintenance'])
-    app.include_router(trace_event.router, tags=['trace_event'])
     return app
