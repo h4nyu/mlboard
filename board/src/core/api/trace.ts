81f7540522b729d03b0b17badb7dcbebddc04c63
908kre <ce51046.higuchi@gmail.com>
2019-08-23 13:41:04 +0000

    add trace

--- board/src/core/api/trace.ts
+++ board/src/core/api/trace.ts
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import { ITrace } from "~/core/models";
 
-
-export const all =  async (
-): Promise<ITrace[]>  => {
+export const all =  async () => {
   const res = await axios.get('api/trace/all');
-  const resData = res.data as {
-    id: string;
-    name: string;
-    value: number;
-  }[];
+  const resData = res.data as string[]
+  return resData;
+}
 
-  return resData.map(x => { 
-    return {
-      id: x.id,
-      name: x.name,
-      value: x.value,
-    };
+export const rangeBy =  async (traceId:string, fromDate:string, toDate:string) => {
+  const res = await axios.get('api/trace/range-by', {
+    id: traceId,
+    from_date:fromDate,
+    to_date:toDate
   });
+  const resData = res.data as {
+    value: number;
+    ts: string
+  }[];
+  return resData 
 };
--- board/src/store/traceStore.ts
+++ board/src/store/traceStore.ts
@@ -6,30 +6,48 @@ import {
   ITrace 
 } from '~/core/models'; 
 import {Map} from 'immutable';
+import uuid from 'uuid';
+import moment from 'moment';
 import * as traceApi from '~/core/api/trace';
 
 export class TraceStore{
-  @observable traceList: string[] =[]
-  @observable selectedIds: string[] = []
+  @observable traceIds: string[] = []
+  @observable fromDate: string = moment().add(-1, 'hours').format()
+  @observable toData: string = moment().format()
   @observable traceMap: Map<string, ITrace> = Map({})
 
 
-  @action setMap = (rows: ITrace[]) => {
-    rows.map(x => this.traceMap.set(x.id, x));
+  @action setTrace = (ITrace) => {
+    this.traceMap.set(x.id, x);
   }
 
-  @action select = (traceId: string) => {
-    if(this.selectedIds.includes(traceId)){
-      this.selectedIds = this.selectedIds.filter(x => x !== traceId);
-    }else{
-      this.selectedIds = [...this.selectedIds, traceId];
+  @action deleteTrace =  (traceId: string) => {
+    this.traceMap.delete(traceId)
+  }
+
+  @action setTraceIds = (rows: string[]) => {
+    this.traceIds = rows
+  }
+
+  select =  async (traceId: string) => {
+    const points = await traceApi.getPoints(traceId, this.fromDate, this.toData);
+    if(points){
+      const trace:ITrace = {
+        id: uuid(),
+        configId: traceId,
+        fromDate: this.fromDate,
+        toData: this.toData,
+        points: points
+      }
+      this.setTrace(trace)
     }
   }
 
-  fetch = async () => {
-    const rows = await traceApi.all();
-    this.setMap(rows);
+  fetchTraceIds = async () => {
+    const rows = await traceApi.getTraceIds();
+    this.setTraceIds(rows);
   }
+
 }
 
 const store = new TraceStore();
