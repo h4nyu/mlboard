79c786a4e58d60efe1a632c51b81fcd273235789
908kre <ce51046.higuchi@gmail.com>
2019-11-04 09:42:05 +0000

    pass test

--- app/mlboard/models/trace.py
+++ app/mlboard/models/trace.py
@@ -15,8 +15,10 @@ class Trace:
         created_at: t.Union[datetime, None] = None,
         updated_at: t.Union[datetime, None] = None,
         id: t.Union[UUID, None] = None,
+        workspace_id: t.Union[UUID, None] = None,
     ) -> None:
         self.id = id if id is not None else uuid4()
+        self.workspace_id = workspace_id if workspace_id is not None else uuid4()
         self.created_at = created_at if created_at is not None else datetime.now()
         self.updated_at = updated_at if updated_at is not None else datetime.now()
         self.tag = tag
