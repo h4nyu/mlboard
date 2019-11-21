bfe8b1751b45bbd3706b0a0a63dad1787511e88d
x1nyuan <yao.ntno@gmail.com>
2019-11-21 13:38:14 +0900

    fix range

--- board/src/index.tsx
+++ board/src/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from '~/connectors/App';
-import { spy } from 'mobx';
 import '~/styles';
 
-spy((event) => {
-  if (event.type === 'action') {
-    console.log(`${event.name} with args: ${event.arguments}`);
-  }
-});
 
 render(<App/>, document.getElementById('app'));
