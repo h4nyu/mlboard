0b77a68e3415809e430bcb4ecab8fd56342360aa
h4nyu <yao.ntno@gmail.com>
2019-12-25 14:54:44 +0000

    tutorial trait

--- app_rust/src/infra/database.rs
+++ app_rust/src/infra/database.rs
@@ -1,60 +1,58 @@
-use chrono::prelude::{Utc, DateTime};
-use postgres::{Client,  NoTls, ToStatement, Row};
-use serde::{Serialize, Deserialize};
+use chrono::prelude::{Utc};
+use postgres::{Client,  NoTls, Row};
+use serde::{Serialize};
 use uuid::Uuid;
 use rayon::prelude::*;
-use crate::repository::{ModelRepository};
+use csv::Writer;
 use crate::models::{Point};
 use std::io::{Write};
-use std::time::{Instant};
 
-fn with_connection<F>(f: F)
+pub fn with_connection<F>(f: F)
     where F:Fn(&mut Client) -> () {
     let mut client = Client::connect("host=db user=mlboard password=mlboard", NoTls).unwrap();
     f(&mut client);
 }
 
-pub struct PointRepository<'a> {
+pub struct Repository<'a> {
     conn: &'a mut Client,
     table_name: &'static str,
 }
 
-impl <'a> PointRepository<'a> {
-    pub fn new(conn: &mut Client) -> PointRepository {
-        return PointRepository{
-            conn: conn,
-            table_name: "points",
-        }
-    }
+pub trait IRepository<T> {
+    fn all(&mut self) -> Vec<T>;
+    fn clear(&mut self);
+    fn bulk_insert(&mut self, rows: &[T]) -> u64;
+}
 
-    fn to_model(row: &Row) -> Point {
+pub trait ConvertTo<T> {
+    fn to(row: &Row) -> T;
+}
+
+impl <'a> ConvertTo<Point> for Repository<'a> {
+    fn to(row: &Row) -> Point {
         Point{
             ts: row.get("ts"),
-            trace_id: row.get("trace_id"),
             value: row.get("value"),
+            trace_id: row.get("trace_id"),
         }
     }
 }
-impl<T, M> ModelRepository<M> for T where T: PointRepository {
-    fn all(&mut self) -> Vec<Point> {
+impl<'a, T> IRepository<T> for Repository<'a> 
+where Self: ConvertTo<T>, T: Serialize
+{
+    fn all(&mut self) -> Vec<T> {
         return self.conn.query::<str>(&format!("SELECT * FROM {}", self.table_name), &[])
             .unwrap()
             .iter()
-            .map(PointRepository::to_model)
-            .collect::<Vec<Point>>();
+            .map(Self::to)
+            .collect::<Vec<T>>();
     }
 
-    fn bulk_insert(&mut self, rows: &[Point]) -> u64 {
-        let mut wtr = self.conn.copy_in::<str>(&format!("COPY {} FROM STDIN CSV", self.table_name)).unwrap();
-        let data = rows.iter()
-            .map(|x| {
-                format!("{ts},{value},{trace_id}\n",
-                        ts=x.ts,
-                        value=x.value,
-                        trace_id=&x.trace_id)
-            })
-            .collect::<Vec<String>>()
-            .join("");
+    fn bulk_insert(&mut self, rows: &[T]) -> u64 {
+        let mut wtr = self.conn.copy_in::<str>(&format!("COPY {} FROM STDIN CSV HEADER", self.table_name)).unwrap();
+        let mut csv_wtr = Writer::from_writer(vec![]);
+        rows.iter().for_each(|x|{csv_wtr.serialize(x).unwrap();});
+        let data = String::from_utf8(csv_wtr.into_inner().unwrap()).unwrap();
         wtr.write_all(data.as_bytes()).unwrap();
         return wtr.finish().unwrap()
     }
@@ -64,37 +62,40 @@ impl<T, M> ModelRepository<M> for T where T: PointRepository {
     }
 }
 
+impl <'a> Repository<'a> {
+    pub fn new(conn: &mut Client) -> Repository {
+        return Repository{
+            conn: conn,
+            table_name: "points",
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    pub fn setup(repo: &mut PointRepository){
+    pub fn setup(repo: &mut dyn IRepository<Point>){
         repo.clear();
     }
 
     #[test]
     fn test_all() {
         let trace_id = Uuid::new_v4();
-        let now = Instant::now();
-        let rows = (0..100)
+        let count = 10000;
+        let rows = (0..count)
             .map(|x| Point{
                 value:x as f64,
                 ts: Utc::now(),
                 trace_id: trace_id,
             })
         .collect::<Vec<Point>>();
-        println!("{:?}", now.elapsed());
-
         with_connection(|client| {
-            let mut repo = PointRepository::new(client);
+            let mut repo = Repository::new(client);
             setup(&mut repo);
-            let now = Instant::now();
-            let count = repo.bulk_insert(&rows);
-            println!("{:?}", now.elapsed());
-            let now = Instant::now();
-            let points = repo.all();
-            println!("{:?}", now.elapsed());
+            let queried_count = repo.bulk_insert(&rows);
+            assert_eq!(queried_count, count);
         })
     }
 }
