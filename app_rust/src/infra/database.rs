fd4a573c5a4fcd2915ba889b02dec3fc2f1dd750
h4nyu <yao.ntno@gmail.com>
2019-12-27 06:00:11 +0000

    dyn dispatch

--- app_rust/src/infra/database.rs
+++ app_rust/src/infra/database.rs
@@ -3,8 +3,6 @@ use chrono::prelude::Utc;
 use postgres::{Client, NoTls, Row};
 use rayon::prelude::*;
 use serde::Serialize;
-use std::io::Write;
-use uuid::Uuid;
 
 pub fn with_connection<F>(f: F)
 where
@@ -14,67 +12,64 @@ where
     f(&mut client);
 }
 
-pub struct PointRepository<'a, T> {
-    conn: &'a mut Client,
-    table_name: &'static str,
-    to_model: fn() -> T,
+// pub struct Table<'a> {
+//     conn: &'a mut Client,
+// }
+
+pub trait Table<T> {
+    fn get_table_name() -> &'static str;
+    fn from_row(row: &Row) -> T;
 }
 
-pub fn to_point() -> Point {
-    Point {
-        ts: Utc::now(),
-        value: 0.1,
-        trace_id: Uuid::new_v4(),
+impl Table<Point> for Client {
+    fn get_table_name() -> &'static str {
+        "points"
+    }
+    fn from_row(row: &Row) -> Point {
+        Point {
+            ts: row.get("ts"),
+            value: row.get("value"),
+            trace_id: row.get("trace_id"),
+        }
     }
 }
 
-pub fn to_trace() -> Trace {
-    Trace {
-        id: Uuid::new_v4(),
-        name: "aaaa".to_string(),
-        created_at: Utc::now(),
-        updated_at: Utc::now(),
+impl Table<Trace> for Client {
+    fn get_table_name() -> &'static str {
+        "traces"
     }
-}
-
-impl<'a> PointRepository<'a, Point> {
-    pub fn new(client: &'a mut Client) -> PointRepository<'a, Point> {
-        return PointRepository::<Point> {
-            conn: client,
-            table_name: "points",
-            to_model: to_point,
-        };
-    }
-}
-
-impl<'a> PointRepository<'a, Trace> {
-    pub fn new(client: &'a mut Client) -> PointRepository<'a, Trace> {
-        return PointRepository::<Trace> {
-            conn: client,
-            table_name: "traces",
-            to_model: to_trace,
-        };
+    fn from_row(row: &Row) -> Trace {
+        Trace {
+            id: row.get("id"),
+            name: row.get("name"),
+            created_at: row.get("created_at"),
+            updated_at: row.get("updated_at"),
+        }
     }
 }
 
 pub trait Repository<T> {
+    fn all(&mut self) -> Vec<T>;
     fn clear(&mut self);
 }
 
-impl<'a> Repository<Trace> for PointRepository<'a, Trace> {
-    fn clear(&mut self) {
-        println!("{:?}", self.table_name);
-        self.conn
-            .execute::<str>(&format!("TRUNCATE TABLE {}", self.table_name), &[])
-            .unwrap();
+impl<T> Repository<T> for Client
+where
+    Self: Table<T>,
+{
+    fn all(&mut self) -> Vec<T> {
+        let table_name = Self::get_table_name();
+        return self
+            .query::<str>(&format!("SELECT * FROM {}", table_name), &[])
+            .unwrap()
+            .iter()
+            .map(Self::from_row)
+            .collect::<Vec<T>>();
     }
-}
 
-impl<'a> Repository<Point> for PointRepository<'a, Point> {
     fn clear(&mut self) {
-        println!("{:?}", self.table_name);
-        self.conn
-            .execute::<str>(&format!("TRUNCATE TABLE {}", self.table_name), &[])
+        let table_name = Self::get_table_name();
+        self.execute::<str>(&format!("TRUNCATE TABLE {}", table_name), &[])
             .unwrap();
     }
 }
@@ -83,21 +78,25 @@ impl<'a> Repository<Point> for PointRepository<'a, Point> {
 mod tests {
     use super::*;
 
-    fn with_point_repo(repo: &mut Repository<Point>) {
-        repo.clear();
-    }
-
-    fn with_trace_repo(repo: &mut Repository<Trace>) {
-        repo.clear();
-    }
+    // fn with_point_repo(repo: &mut Repository<Point>) {
+    //     repo.clear();
+    // }
+    //
+    // fn with_trace_repo(repo: &mut Repository<Trace>) {
+    //     repo.clear();
+    // }
 
     #[test]
     fn test_all() {
         with_connection(|client| {
-            let mut repo_point = PointRepository::<Point>::new(client);
-            repo_point.clear();
-            let mut repo_trace = PointRepository::<Trace>::new(client);
-            repo_trace.clear();
+            let trace_repo = client as &mut dyn Repository<Point>;
+            trace_repo.all();
+            let point_repo = client as &mut dyn Repository<Trace>;
+            point_repo.all();
+            // let mut repo_point = PointRepository::<Point>::new(client);
+            // repo_point.clear();
+            // let mut repo_trace = PointRepository::<Trace>::new(client);
+            // repo_trace.clear();
             // with_point_repo(&mut repo);
             // with_trace_repo(&mut repo);
         })
