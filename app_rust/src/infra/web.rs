31db202682ffbcc04082666dcc218db341392ea5
h4nyu <yao.ntno@gmail.com>
2019-12-30 11:27:12 +0000

    add E2E test

--- app_rust/src/infra/web.rs
+++ app_rust/src/infra/web.rs
@@ -2,12 +2,15 @@ use crate::domain::entities::{Point, Trace};
 use crate::domain::usecase::PointService;
 use crate::domain::Repository;
 use actix_web::{get, web, App, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
 
-async fn index() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("plain/text")
-        .header("X-Hdr", "sample")
-        .body("data")
+#[derive(Debug, Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+    number: i32,
+}
+fn index(item: web::Json<MyObj>) -> HttpResponse {
+    HttpResponse::Ok().json(item.0) // <- send respons
 }
 
 pub async fn run() -> std::io::Result<()> {
@@ -20,14 +23,31 @@ pub async fn run() -> std::io::Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use actix_web::dev::Service;
     use actix_web::{http, test};
 
     #[actix_rt::test]
     async fn test_index_ok() {
-        let req = test::TestRequest::with_header("content-type", "text/plain").to_http_request();
+        let mut app =
+            test::init_service(App::new().service(web::resource("/").route(web::post().to(index))))
+                .await;
 
-        let resp = index().await;
-        println!("{:?}", resp);
+        let req = test::TestRequest::post()
+            .uri("/")
+            .set_json(&MyObj {
+                name: "my-name".to_owned(),
+                number: 43,
+            })
+            .to_request();
+        let resp = app.call(req).await.unwrap();
+
         assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(response_body, r##"{"name":"my-name","number":43}"##);
     }
 }
