5b6877e20d60602a94de56f61e6e701149da085c
h4nyu <yao.ntno@gmail.com>
2020-01-22 14:16:08 +0000

    tmp

--- app_rust/src/infra/database.rs
+++ app_rust/src/infra/database.rs
@@ -1,29 +1,16 @@
 use crate::domain::entities::{Point, Trace};
 // use crate::domain::{PointRepository, Repository, TraceRepository, Transition};
+use csv::Writer;
+use serde::Serialize;
 use chrono::prelude::{DateTime, Utc};
-use tokio_postgres::{NoTls, Client};
+use tokio_postgres::{NoTls, Client, Row, CopyInSink};
 use std::error::Error;
 use tokio::runtime::Runtime;
-// use csv::Writer;
-// use postgres::{types::ToSql, Client, NoTls, Row};
-// use rayon::prelude::*;
-// use serde::Serialize;
-// use std::error::Error;
-// use std::io::Write;
-// use std::sync::{Arc, Mutex};
-// use uuid::Uuid;
-// pub struct Postgresql(Arc<Mutex<Client>>);
-// pub struct QueryValue<'a>(&'static str, &'a dyn ToSql);
+use std::fmt::Debug;
+use uuid::Uuid;
 
 
 
-
-// pub trait SQLable {
-//     fn table_name() -> &'static str;
-//     fn select() -> &'static str;
-//     fn copy_in() -> &'static str;
-//     fn from_row(row: &Row) -> Self;
-// }
 //
 // struct Postgresql;
 // impl Postgresql {
@@ -36,24 +23,25 @@ use tokio::runtime::Runtime;
 //     }
 // }
 //
-// impl SQLable for Point {
-//     fn table_name() -> &'static str {
-//         "points"
-//     }
-//     fn select() -> &'static str {
-//         "SELECT * FROM points"
-//     }
-//     fn copy_in() -> &'static str {
-//         "COPY points FROM STDIN CSV HEADER"
-//     }
-//     fn from_row(row: &Row) -> Self {
-//         Self {
-//             value: row.get("value"),
-//             trace_id: row.get("trace_id"),
-//             ts: row.get("ts"),
-//         }
-//     }
-// }
+impl SQLable for Point {
+    fn table_name() -> &'static str {
+        "points"
+    }
+
+    fn select() -> &'static str {
+        "SELECT * FROM points"
+    }
+    fn copy_in() -> &'static str {
+        "COPY points FROM STDIN CSV HEADER"
+    }
+    fn from_row(row: &Row) -> Self {
+        Self {
+            value: row.get("value"),
+            trace_id: row.get("trace_id"),
+            ts: row.get("ts"),
+        }
+    }
+}
 //
 // impl SQLable for Trace {
 //     fn select() -> &'static str {
@@ -154,32 +142,95 @@ use tokio::runtime::Runtime;
 //
 //
 //
+//
+pub trait SQLable {
+    fn table_name() -> &'static str;
+    fn select() -> &'static str;
+    fn copy_in() -> &'static str;
+    fn from_row(row: &Row) -> Self;
+}
+
 struct Conn {
     client:Client
 }
+
 impl Conn {
     pub async fn new() -> Result<Self, Box<Error>> {
         let (client, connection) = tokio_postgres::connect(
             "host=db user=mlboard password=mlboard", 
             NoTls
         ).await?;
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+
         Ok(Conn{
             client: client,
         })
     }
+
+    pub async fn all<T>(&self) -> Result<Vec<T>, Box<Error>> 
+        where T:SQLable + Debug
+    {
+        let sql = format!("SELECT * FROM {}", T::table_name());
+        let rows:Vec<T> = self.client
+            .query(&sql[..], &[])
+            .await?
+            .iter()
+            .map(T::from_row)
+            .collect();
+        Ok(vec![])
+    }
+
+    pub async fn bulk_insert<T>(&self, rows: &[T]) -> Result<u64, Box<Error>> 
+        where T:SQLable + Debug + Serialize,
+    {
+        let mut csv_wtr = Writer::from_writer(vec![]);
+        rows.iter().for_each(|x| {
+            csv_wtr.serialize(x).unwrap();
+        });
+        let data = String::from_utf8(csv_wtr.into_inner().unwrap()).unwrap();
+
+        let sink:i32 = self.client
+            .copy_in(&T::copy_in()[..]).await?.finish();
+        Ok(0)
+        // copy_in_writer.write_all(data.as_bytes()).unwrap();
+        //
+        // return copy_in_writer.finish().unwrap();
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use async_std::{fs::File, io, prelude::*, task};
     use tokio;
-    #[test]
-    fn test_transtion() -> Result<(), Box<dyn Error>> {
-        let mut rt = Runtime::new()?;
-        rt.block_on(async {
-            let conn = Conn::new().await?;
-            Ok(())
-        })
+
+    #[tokio::test]
+    async fn test_all() -> Result<(), Box<dyn Error>> {
+        let conn = Conn::new().await?;
+        let points: Vec<Point> = conn.all().await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_bulk_insert() -> Result<(), Box<dyn Error>> {
+        let conn = Conn::new().await?;
+        let trace_id = Uuid::new_v4();
+        let count = 100;
+        let rows:Vec<Point> = (0..100)
+            .map(|x| {Point{
+                trace_id: trace_id,
+                ts:Utc::now(),
+                value: 0.0
+            }})
+            .collect();
+
+        let inserted = conn.bulk_insert(&rows).await?;
+        assert_eq!(inserted, count);
+        Ok(())
     }
 }
